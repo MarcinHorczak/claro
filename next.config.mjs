20a25478406e2881e7cefa8fbaf7ca01fb69ace4
Marcin Horczak <marcin.horczak@euvic.pl>
2024-10-18 17:30:01 +0200

    update build and deploy configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/",
 };
 
 export default nextConfig;
