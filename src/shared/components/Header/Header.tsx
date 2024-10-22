5fa6945d1a06e0a86df012a6d659dc9dab90c6e9
Marcin Horczak <marcin.horczak@euvic.pl>
2024-10-22 09:33:09 +0200

    chore: logo-image optimization

--- src/shared/components/Header/Header.tsx
+++ src/shared/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = () => (
   <div className="flex flex-wrap content-center justify-center gap-10 p-10">
     <div className="bg-primary flex h-20 w-20 items-center justify-center rounded-full">
       <div>
-        <Image src="/logo.png" alt="Logo" priority height={40} width={40} />
+        <Image src="/logo.svg" alt="Logo" priority height={40} width={40} />
       </div>
     </div>
     <Navigation />
