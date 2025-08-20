b7d768dca4b116cdc8cba230a9200c21426395e0
Marcin Horczak <marcin.horczak@euvic.pl>
2025-08-20 12:48:25 +0200

    fix: not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,19 @@
-const NotFound = () => <div>404</div>;
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import { Paths } from "@utils/paths";
+
+const NotFound = () => {
+  const t = useTranslations();
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p className="text-lg">{t("notFound.description")}</p>
+      <Link href={Paths.Home} className="text-lg underline">
+        {t("notFound.backToHome")}
+      </Link>
+    </div>
+  );
+};
 
 export default NotFound;
