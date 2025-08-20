046f4f5eb06f427ba13a4e355db50db06a263d48
Marcin Horczak <marcin.horczak@euvic.pl>
2025-08-20 21:59:37 +0200

    fix: header behavior rwd

--- src/components/custom/Header/Logo/Logo.tsx
+++ src/components/custom/Header/Logo/Logo.tsx
@@ -10,26 +10,44 @@ import LogoIcon from "/public/logo.svg";
 
 export const Logo = () => {
   const t = useTranslations();
-  const showText = useBreakpoint("md");
+  const isDesktop = useBreakpoint("md");
   const [{ y }] = useWindowScroll();
   const isScrolled = !!y && y > 0;
 
-  const textStyles = isScrolled ? "opacity-0" : "opacity-100";
-  const logoStyles = isScrolled ? "translate-y-3" : "";
+  const logoContainerStyles = isDesktop ? "flex-col gap-1" : "flex-row gap-3";
+  const logoTransform = isDesktop && isScrolled ? "translate-y-3" : "";
 
   return (
-    <div className="flex flex-col items-center gap-1">
+    <div
+      className={`relative flex items-center transition-all duration-500 ease-in-out ${logoContainerStyles}`}
+    >
       <Link href={Paths.Home}>
         <div
-          className={`flex h-11 w-11 items-center justify-center rounded-full bg-primary transition-all duration-500 ease-in-out ${logoStyles}`}
+          className={`flex h-11 w-11 items-center justify-center rounded-full bg-primary transition-all duration-500 ease-in-out ${logoTransform}`}
         >
           <Image src={LogoIcon} alt="Logo" priority className="w-5" />
         </div>
       </Link>
-      {showText && (
-        <p
-          className={`text-sm tracking-widest text-primary transition-all duration-500 ease-in-out ${textStyles}`}
-        >
+
+      {isDesktop ? (
+        <div className="relative">
+          <p
+            className={`text-sm tracking-widest text-primary transition-all duration-500 ease-in-out ${
+              isScrolled ? "opacity-0" : "opacity-100"
+            }`}
+          >
+            {t("name")}
+          </p>
+          <p
+            className={`absolute text-sm tracking-widest text-primary transition-all duration-500 ease-in-out ${
+              isScrolled ? "opacity-100" : "opacity-0"
+            } ${isScrolled ? "-top-6 left-16" : "left-0 top-0"}`}
+          >
+            {t("name")}
+          </p>
+        </div>
+      ) : (
+        <p className="text-sm tracking-widest text-primary transition-all duration-500 ease-in-out">
           {t("name")}
         </p>
       )}
