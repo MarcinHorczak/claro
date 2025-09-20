aaddadfe6cc936e9990c6ce19ed1f0d078739bcd
Marcin Horczak <marcin.horczak@euvic.pl>
2025-09-20 13:45:49 +0200

    fix: decoration polygon

--- src/components/custom/ContentContainer/PolygonClip/PolygonClip.tsx
+++ src/components/custom/ContentContainer/PolygonClip/PolygonClip.tsx
@@ -11,14 +11,15 @@ export const PolygonClip = ({
   bgColor,
   variant,
 }: PolygonClipProps) => (
-  <div className="relative" style={{ height: `${polygonHeight}` }}>
+  <div className="relative" style={{ height: `${polygonHeight * 4}px` }}>
     <div
       className={`absolute w-full ${variant === "top" ? "bottom-0" : "top-0"}`}
+      style={{ height: `${polygonHeight * 2}px` }}
     >
       <div
         className={`absolute ${variant === "top" ? "bottom-0" : "top-0"} w-full ${bgColor} opacity-40`}
         style={{
-          height: `${polygonHeight / 2}`,
+          height: `${polygonHeight * 2}px`,
           clipPath: cn(
             variant === "top" && "polygon(0 100%, 100% 0%, 100% 100%, 0% 100%)",
             variant === "bottom" && "polygon(0 0%, 100% 0%, 100% 100%, 0% 0%)",
@@ -28,11 +29,12 @@ export const PolygonClip = ({
     </div>
     <div
       className={`absolute w-full ${variant === "top" ? "bottom-0" : "top-0"}`}
+      style={{ height: `${polygonHeight * 4}px` }}
     >
       <div
         className={`absolute ${variant === "top" ? "bottom-0" : "top-0"} w-full ${bgColor} opacity-30`}
         style={{
-          height: `${polygonHeight}`,
+          height: `${polygonHeight * 4}px`,
           clipPath: cn(
             variant === "top" && "polygon(0 100%, 100% 0%, 100% 100%, 0% 100%)",
             variant === "bottom" && "polygon(0 0%, 100% 0%, 100% 100%, 0% 0%)",
